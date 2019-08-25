9585463901fdb0da75a92e1a6ef311f1a7e06112
Robert S <rsavian@nerdery.com>
2019-08-25 00:04:53 -0500

    test

--- src/views/home-page/HomePage.tsx
+++ src/views/home-page/HomePage.tsx
@@ -1,7 +1,7 @@
 import styles from './HomePage.module.scss';
 
 import * as React from 'react';
-import {connect, DispatchProp} from 'react-redux';
+import { connect, DispatchProp } from 'react-redux';
 import IAction from '../../models/IAction';
 import IStore from '../../models/IStore';
 
@@ -12,15 +12,9 @@ interface IStateToProps {}
 const mapStateToProps = (state: IStore, ownProps: IProps): IStateToProps => ({});
 
 class HomePage extends React.Component<IProps & IStateToProps & DispatchProp<IAction<any>>, IState> {
-
-    public render(): JSX.Element {
-        return (
-            <div className={styles.wrapper}>
-                Home page
-            </div>
-        );
-    }
-
+  public render(): JSX.Element {
+    return <div className={styles.wrapper}>Home page</div>;
+  }
 }
 
 export { HomePage as Unconnected };
