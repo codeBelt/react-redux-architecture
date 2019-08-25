61d1f2486557ddee65ca5e7f6a46a8201a7954d0
Robert S <rsavian@nerdery.com>
2019-08-25 00:18:38 -0500

    add semantic-ui-react

--- src/views/home-page/HomePage.tsx
+++ src/views/home-page/HomePage.tsx
@@ -4,6 +4,26 @@ import * as React from 'react';
 import { connect, DispatchProp } from 'react-redux';
 import IAction from '../../models/IAction';
 import IStore from '../../models/IStore';
+import { Card, Icon, Image } from 'semantic-ui-react';
+
+const CardExampleCard = () => (
+  <Card>
+    <Image src="" wrapped={true} ui={false} />
+    <Card.Content>
+      <Card.Header>Matthew</Card.Header>
+      <Card.Meta>
+        <span className="date">Joined in 2015</span>
+      </Card.Meta>
+      <Card.Description>Matthew is a musician living in Nashville.</Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a href="/">
+        <Icon name="user" />
+        22 Friends
+      </a>
+    </Card.Content>
+  </Card>
+);
 
 interface IProps {}
 interface IState {}
@@ -13,7 +33,12 @@ const mapStateToProps = (state: IStore, ownProps: IProps): IStateToProps => ({})
 
 class HomePage extends React.Component<IProps & IStateToProps & DispatchProp<IAction<any>>, IState> {
   public render(): JSX.Element {
-    return <div className={styles.wrapper}>Home page</div>;
+    return (
+      <div className={styles.wrapper}>
+        Home page
+        <CardExampleCard />
+      </div>
+    );
   }
 }
 
