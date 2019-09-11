4497cea54116b94ce0c48004b3ece8c717040e4c
Robert S <rsavian@nerdery.com>
2019-09-11 11:30:24 -0500

    make more readable and add comments

--- src/stores/error/ErrorReducer.js
+++ src/stores/error/ErrorReducer.js
@@ -13,6 +13,7 @@ export default class ErrorReducer {
      * Removes a specific error by it's id.
      */
     if (type === ErrorAction.REMOVE) {
+      // Create a new state without the error that has the same id as the payload
       return Object.entries(state).reduce((newState, [key, value]) => {
         if (value.id !== payload) {
           newState[key] = value;
@@ -30,27 +31,23 @@ export default class ErrorReducer {
     }
 
     /*
+     * If the action type has the key word '_FINISHED' then the start action is complete
+     */
+    const isFinishedRequestType = type.includes('_FINISHED');
+    /*
      * If the action type has the key word 'REQUEST_' and not '_FINISHED' then we
      * want to remove the old error because there is a new request happening.
      */
-    const isStartRequestType = [type.includes('REQUEST_') === true, type.includes('_FINISHED') === false].every(Boolean);
+    const isStartRequestType = type.includes('REQUEST_') && !isFinishedRequestType;
 
     if (isStartRequestType === true) {
-      return Object.entries(state).reduce((newState, [key, value]) => {
-        if (key !== `${type}_FINISHED`) {
-          newState[key] = value;
-        }
+      // If there is already an error we want to remove it if the start action is triggered.
+      const { [`${type}_FINISHED`]: value, ...statWithoutFinishedType } = state;
 
-        return newState;
-      }, {});
+      return statWithoutFinishedType;
     }
 
-    /*
-     * If the action type has the key word '_FINISHED' we use the type as the key value
-     * for the error.
-     */
-    const isFinishedRequestType = type.includes('_FINISHED');
-    const isError = [isFinishedRequestType, error].every(Boolean);
+    const isError = isFinishedRequestType && error;
 
     if (isError === false) {
       return state;
