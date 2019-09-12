970cfbfa41f37060b0ea97c7b75a875e9b36eeee
Robert S <rsavian@nerdery.com>
2019-09-12 16:01:44 -0500

    Make component instead of multiple functions

---
+++ src/views/episodes-page/components/episodes-table-row/EpisodesTableRow.jsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Image, Table } from 'semantic-ui-react';
+
+export default class EpisodesTableRow extends React.PureComponent {
+  render() {
+    const { rowData } = this.props;
+
+    return (
+      <Table.Row key={rowData.episode}>
+        <Table.Cell>
+          <Image src={rowData.image} rounded={true} size="small" />
+        </Table.Cell>
+        <Table.Cell>{rowData.episode}</Table.Cell>
+        <Table.Cell>{rowData.date}</Table.Cell>
+        <Table.Cell>{rowData.name}</Table.Cell>
+      </Table.Row>
+    );
+  }
+}
