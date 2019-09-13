121d9a223d1c11872ec5d09fddaf458a62be052a
Robert S <rsavian@nerdery.com>
2019-09-12 21:00:33 -0500

    Create small components

---
+++ src/views/episodes-page/components/episodes-table-row/EpisodesTableRow.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { Image, Table } from 'semantic-ui-react';
+import IEpisodeTableRow from '../../../../selectors/episodes/models/IEpisodeTableRow';
+
+interface IProps {
+  readonly rowData: IEpisodeTableRow;
+}
+interface IState {}
+
+export default class EpisodesTableRow extends React.PureComponent<IProps, IState> {
+  public render(): JSX.Element {
+    const { rowData } = this.props;
+
+    return (
+      <Table.Row key={rowData.episode}>
+        <Table.Cell>
+          <Image src={rowData.image} rounded={true} size="small" />
+        </Table.Cell>
+        <Table.Cell>{rowData.episode}</Table.Cell>
+        <Table.Cell>{rowData.date}</Table.Cell>
+        <Table.Cell>{rowData.name}</Table.Cell>
+      </Table.Row>
+    );
+  }
+}
