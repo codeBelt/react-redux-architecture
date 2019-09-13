53c94c59a70151c3b0fd44eda9ff0fb61b1956b5
Robert S <rsavian@nerdery.com>
2019-09-13 07:22:52 -0500

    Update BaseReducer types

--- src/utilities/BaseReducer.ts
+++ src/utilities/BaseReducer.ts
@@ -1,13 +1,17 @@
 import IAction from '../models/IAction';
 
+type Handler<T> = (state: T, action: IAction<any>) => T;
+
 export default abstract class BaseReducer<T> {
   public initialState: T = {} as any;
 
   public reducer = (state: T = this.initialState, action: IAction<any>): T => {
-    if (action.error || !this[action.type]) {
+    const handler: Handler<T> | undefined = this[action.type];
+
+    if (action.error || !handler) {
       return state;
     }
 
-    return this[action.type](state, action);
+    return handler(state, action);
   };
 }
