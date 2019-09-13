82528b3677490c6a73f94ccd8b086d03d9425e1b
Robert S <rsavian@nerdery.com>
2019-09-13 17:14:59 -0500

    Change endpoint replace string format to match express

--- src/stores/shows/ShowsEffect.ts
+++ src/stores/shows/ShowsEffect.ts
@@ -9,19 +9,19 @@ import EffectUtility from '../../utilities/EffectUtility';
 
 export default class ShowsEffect {
   public static async requestShow(showId: string): Promise<ShowModel | HttpErrorResponseModel> {
-    const endpoint: string = environment.api.shows.replace('{showId}', showId);
+    const endpoint: string = environment.api.shows.replace(':showId', showId);
 
     return EffectUtility.getToModel<ShowModel>(ShowModel, endpoint);
   }
 
   public static async requestEpisodes(showId: string): Promise<EpisodeModel[] | HttpErrorResponseModel> {
-    const endpoint: string = environment.api.episodes.replace('{showId}', showId);
+    const endpoint: string = environment.api.episodes.replace(':showId', showId);
 
     return EffectUtility.getToModel<EpisodeModel[]>(EpisodeModel, endpoint);
   }
 
   public static async requestCast(showId: string): Promise<CastModel[] | HttpErrorResponseModel> {
-    const endpoint: string = environment.api.cast.replace('{showId}', showId);
+    const endpoint: string = environment.api.cast.replace(':showId', showId);
 
     // Below is just to show you what the above "requestEpisodes" method is doing with "EffectUtility.getToModel".
     // In your application you can change this to match the "requestEpisodes" method.
