68a3434c5da6ee4fdd11f3b694d6f305d79ead00
Robert S <rsavian@nerdery.com>
2019-09-16 09:46:16 -0500

    add comments

--- src/utilities/BaseReducer.ts
+++ src/utilities/BaseReducer.ts
@@ -1,17 +1,23 @@
 import IAction from '../models/IAction';
 
-type Handler<T> = (state: T, action: IAction<any>) => T;
+type ReducerMethod<T> = (state: T, action: IAction<any>) => T;
 
 export default abstract class BaseReducer<T> {
   public initialState: T = {} as any;
 
   public reducer = (state: T = this.initialState, action: IAction<any>): T => {
-    const handler: Handler<T> | undefined = this[action.type];
+    // if the action type is used for a method name then this be a reference to
+    // that class method.
+    // if the action type is not found then the "method" const will be undefined.
+    const method: ReducerMethod<T> | undefined = this[action.type];
 
-    if (!handler || action.error) {
+    // if the action type "method" const is undefined or the action is an error
+    // return the state.
+    if (!method || action.error) {
       return state;
     }
 
-    return handler(state, action);
+    // Return the modified state from the method.
+    return method(state, action);
   };
 }
