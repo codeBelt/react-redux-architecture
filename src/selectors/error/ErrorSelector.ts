91aff3f6afa77a0eede5b47d2eb408c7886b5cda
Robert S <rsavian@nerdery.com>
2019-09-26 17:26:58 -0500

    add comments

--- src/selectors/error/ErrorSelector.ts
+++ src/selectors/error/ErrorSelector.ts
@@ -1,24 +1,36 @@
 import { createSelector, ParametricSelector } from 'reselect';
 import IErrorState from '../../stores/error/models/IErrorState';
 import IStore from '../../models/IStore';
+import HttpErrorResponseModel from '../../models/HttpErrorResponseModel';
 
 export class ErrorSelector {
+  /**
+   * Returns a new object with the keys being the finished action type
+   * (e.g. "SomeAction.REQUEST_*_FINISHED") and the value being a
+   * HttpErrorResponseModel.
+   */
   public static selectRawErrors(errorState: IErrorState, actionTypes: string[]): IErrorState {
     return actionTypes.reduce((partialState: object, actionType: string) => {
-      if (errorState[actionType]) {
-        partialState[actionType] = errorState[actionType];
+      const model: HttpErrorResponseModel = errorState[actionType];
+
+      if (model) {
+        partialState[actionType] = model;
       }
 
       return partialState;
     }, {});
   }
 
+  /**
+   * Finds any errors matching the array of actionTypes and combines all error
+   * messages in to a single string.
+   */
   public static selectErrorText(errorState: IErrorState, actionTypes: string[]): string {
-    const partialErrorState = ErrorSelector.selectRawErrors(errorState, actionTypes) as IErrorState;
-
     const errorList: string[] = actionTypes.reduce((errorMessages: string[], actionType: string) => {
-      if (partialErrorState[actionType]) {
-        const { message, errors } = partialErrorState[actionType];
+      const model: HttpErrorResponseModel = errorState[actionType];
+
+      if (model) {
+        const { message, errors } = model;
         const arrayOfErrors: string[] = errors.length ? errors : [message];
 
         return errorMessages.concat(arrayOfErrors);
@@ -30,6 +42,9 @@ export class ErrorSelector {
     return errorList.join(', ');
   }
 
+  /**
+   * Returns true or false if there are errors found matching the array of actionTypes.
+   */
   public static hasErrors(errorState: IErrorState, actionTypes: string[]): boolean {
     return actionTypes.map((actionType: string) => errorState[actionType]).filter(Boolean).length > 0;
   }
