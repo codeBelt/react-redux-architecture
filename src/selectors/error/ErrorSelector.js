c30248baba5954773eb41b6d6312888af6f06277
Robert S <rsavian@nerdery.com>
2019-09-26 17:07:10 -0500

    ErrorSelector jsDocs

--- src/selectors/error/ErrorSelector.js
+++ src/selectors/error/ErrorSelector.js
@@ -1,6 +1,25 @@
 import { createSelector } from 'reselect';
 
+/**
+ * @typedef {Object.<string, HttpErrorResponseModel>} IErrorState
+ */
+
+/**
+ * ErrorSelector
+ *
+ * @static
+ */
 export class ErrorSelector {
+  /**
+   * Creates a new object with the key being the finished action type
+   * (e.g. "SomeAction.REQUEST_*_FINISHED") and the value being the
+   * HttpErrorResponseModel.
+   *
+   * @param {IErrorState} errorState
+   * @param {string[]} actionTypes
+   * @returns {IErrorState}
+   * @static
+   */
   static selectRawErrors(errorState, actionTypes) {
     return actionTypes.reduce((partialState, actionType) => {
       if (errorState[actionType]) {
@@ -11,12 +30,19 @@ export class ErrorSelector {
     }, {});
   }
 
+  /**
+   * Finds any errors matching the array of actionTypes and combines all error
+   * messages in to a single string.
+   *
+   * @param {IErrorState} errorState
+   * @param {string[]} actionTypes
+   * @returns {string}
+   * @static
+   */
   static selectErrorText(errorState, actionTypes) {
-    const partialErrorState = ErrorSelector.selectRawErrors(errorState, actionTypes);
-
     const errorList = actionTypes.reduce((errorMessages, actionType) => {
-      if (partialErrorState[actionType]) {
-        const { message, errors } = partialErrorState[actionType];
+      if (errorState[actionType]) {
+        const { message, errors } = errorState[actionType];
         const arrayOfErrors = errors.length ? errors : [message];
 
         return errorMessages.concat(arrayOfErrors);
@@ -28,6 +54,14 @@ export class ErrorSelector {
     return errorList.join(', ');
   }
 
+  /**
+   * Returns true or false if there are errors found matching the array of actionTypes.
+   *
+   * @param {IErrorState} errorState
+   * @param {string[]} actionTypes
+   * @returns {boolean}
+   * @static
+   */
   static hasErrors(errorState, actionTypes) {
     return actionTypes.map((actionType) => errorState[actionType]).filter(Boolean).length > 0;
   }
