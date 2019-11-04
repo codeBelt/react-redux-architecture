7d05e4be381385fe1fa35bc03df798485f2238f2
Robert S <rsavian@nerdery.com>
2019-11-04 12:45:22 -0600

    add ToastCard

--- src/views/components/toasts/Toasts.jsx
+++ src/views/components/toasts/Toasts.jsx
@@ -1,28 +1,11 @@
 import styles from './Toasts.module.scss';
 
-import React, { useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import * as ToastsAction from '../../../stores/toasts/ToastsAction';
-import { Card, Button } from 'semantic-ui-react';
-import ToastStatusEnum from '../../../constants/ToastStatusEnum';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import ToastCard from '../toast-card/ToastCard';
 
 export default function Toasts(props) {
-  const dispatch = useDispatch();
-
-  const buttonColorMap = {
-    [ToastStatusEnum.Error]: 'red',
-    [ToastStatusEnum.Warning]: 'orange',
-    [ToastStatusEnum.Success]: 'green',
-  };
-
   const toasts = useSelector((state) => state.toasts.items);
-
-  const onClickRemoveNotification = useCallback(
-    (id) => (event, data) => {
-      dispatch(ToastsAction.removeById(id));
-    },
-    [dispatch]
-  );
 
   if (toasts.length === 0) {
     return null;
@@ -30,23 +13,9 @@ export default function Toasts(props) {
 
   return (
     <div className={styles.wrapper}>
-      {toasts.map((item) => {
-        const buttonColor = buttonColorMap[item.type];
-
-        return (
-          <Card key={item.id}>
-            <Card.Content>
-              <Card.Header content={item.type} />
-              <Card.Description content={item.message} />
-            </Card.Content>
-            <Card.Content extra={true}>
-              <Button color={buttonColor} onClick={onClickRemoveNotification(item.id)}>
-                Close
-              </Button>
-            </Card.Content>
-          </Card>
-        );
-      })}
+      {toasts.map((model) => (
+        <ToastCard key={model.id} item={model} />
+      ))}
     </div>
   );
 }
