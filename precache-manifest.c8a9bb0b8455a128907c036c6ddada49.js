a201780254957eacfc36cc76545c22aca0c8c390
Robert S <rsavian@nerdery.com>
2019-11-07 09:17:37 -0600

    Updates

---
+++ precache-manifest.c8a9bb0b8455a128907c036c6ddada49.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79be9d6d6a3e27c21129d15206466faf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "696af441dc9db87748c9",
+    "url": "/static/css/2.55b2ffa3.chunk.css"
+  },
+  {
+    "revision": "54e858b0dfdf92695085",
+    "url": "/static/css/3.028c9c03.chunk.css"
+  },
+  {
+    "revision": "a5368ad39c6bf5b6814f",
+    "url": "/static/css/4.95322d16.chunk.css"
+  },
+  {
+    "revision": "f1a6dbacb16984a1df38",
+    "url": "/static/css/6.be269c2b.chunk.css"
+  },
+  {
+    "revision": "922301f18c414fcc5325",
+    "url": "/static/css/main.facce1e4.chunk.css"
+  },
+  {
+    "revision": "696af441dc9db87748c9",
+    "url": "/static/js/2.9b0cc2c6.chunk.js"
+  },
+  {
+    "revision": "54e858b0dfdf92695085",
+    "url": "/static/js/3.29af2a1d.chunk.js"
+  },
+  {
+    "revision": "a5368ad39c6bf5b6814f",
+    "url": "/static/js/4.92f1e46c.chunk.js"
+  },
+  {
+    "revision": "0783078b1ceba5d19e50",
+    "url": "/static/js/5.a7655371.chunk.js"
+  },
+  {
+    "revision": "f1a6dbacb16984a1df38",
+    "url": "/static/js/6.f832815b.chunk.js"
+  },
+  {
+    "revision": "74bd17990f0e45312f41",
+    "url": "/static/js/7.2780fd6b.chunk.js"
+  },
+  {
+    "revision": "922301f18c414fcc5325",
+    "url": "/static/js/main.2142b9c5.chunk.js"
+  },
+  {
+    "revision": "40908bc5c3af016706a6",
+    "url": "/static/js/runtime-main.9a0e6a80.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-redux-architecture/precache-manifest.399d2851745d57ccdd11c7132ce8169f.js"
+  "/precache-manifest.c8a9bb0b8455a128907c036c6ddada49.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-redux-architecture/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
