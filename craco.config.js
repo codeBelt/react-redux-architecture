a998c599824ca4f5d7217f448df0ca5df670d5bd
Robert S <rsavian@nerdery.com>
2019-12-11 11:53:35 -0600

    Update to Create React App 3.3.0

--- craco.config.js
+++ craco.config.js
@@ -2,12 +2,6 @@ const path = require('path');
 
 module.exports = function({ env, paths }) {
   return {
-    babel: {
-      presets: [],
-      plugins: ['@babel/plugin-proposal-optional-chaining', '@babel/plugin-proposal-nullish-coalescing-operator'],
-      // loaderOptions: { /* Any babel-loader configuration options: https://github.com/babel/babel-loader. */ },
-      // loaderOptions: (babelLoaderOptions, { env, paths }) => { return babelLoaderOptions; }
-    },
     webpack: {
       alias: {
         environment: path.join(__dirname, 'src', 'environments', process.env.CLIENT_ENV),
