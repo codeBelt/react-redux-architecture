7cf5b434ad511f325f5822e4d2446b2cb6eebf2d
Robert S <rsavian@nerdery.com>
2019-12-31 10:32:35 -0600

    update packages

--- src/stores/rootStore.ts
+++ src/stores/rootStore.ts
@@ -14,7 +14,7 @@ export default function rootStore(initialState: Partial<IStore>, history: Histor
     Boolean
   );
 
-  const store: Store<IStore> = createStore(rootReducer(history), initialState, composeWithDevTools(applyMiddleware(...middleware)));
+  const store: Store<IStore> = createStore(rootReducer(history), initialState as any, composeWithDevTools(applyMiddleware(...middleware)));
 
   // store.subscribe(() => console.log(store.getState()));
 
