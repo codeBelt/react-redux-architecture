0332ed1719ce1def1885dd04d8540b95eaf6e719
Robert S <rsavian@nerdery.com>
2020-01-07 09:21:36 -0600

    fix environment types being any and clean up definitions

---
+++ src/definitions/RecursivePartial.d.ts
@@ -0,0 +1,3 @@
+type RecursivePartial<T> = {
+  [P in keyof T]?: T[P] extends (infer U)[] ? RecursivePartial<U>[] : T[P] extends object ? RecursivePartial<T[P]> : T[P];
+};
--- src/typings.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'redux-freeze';
-declare module 'lodash.groupby';
-
-declare module 'environment' {
-  import baseEnv from './environments/base';
-  const value: ReturnType<typeof baseEnv>;
-
-  export default value;
-}
